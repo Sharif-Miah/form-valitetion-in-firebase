b7b4ae04e003a99c08c76df7e4ec148a9d7a52a2
farhan sharif <farhansharif1410@gmail.com>
2022-10-14 21:10:05 +0530

    firebase uses

--- src/component/FormValitation/FormValitation.jsx
+++ src/component/FormValitation/FormValitation.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import app from '../firebase/firebase.init';
 
-
+const auth = getAuth(app)
 const FormValitation = () => {
 
     const handleSubmitBtn = event => {
         event.preventDefault()
-        const email = event.target.value;
-        const password = event.target.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
         console.log(email, password);
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.log('error', error);
+            })
     }
 
     return (
         <div className='w-50 mx-auto'>
-            <Form >
+            <Form onSubmit={handleSubmitBtn}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control onBlur={handleSubmitBtn} type="email" placeholder="Enter email" name='email' />
+                    <Form.Control type="email" placeholder="Enter email" name='email' />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control onBlur={handleSubmitBtn} type="password" placeholder="Password" name='password' />
+                    <Form.Control type="password" placeholder="Password" name='password' />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
